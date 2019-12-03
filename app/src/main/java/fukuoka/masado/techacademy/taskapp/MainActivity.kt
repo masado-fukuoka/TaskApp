acd4fbcf51e81318d9c20c88fd1b70f81132909b
Masado Fukuoka <masado@eurovision.co.jp>
2019-12-03 20:42:11 +0900

    Initial commit

---
+++ app/src/main/java/fukuoka/masado/techacademy/taskapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package fukuoka.masado.techacademy.taskapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
